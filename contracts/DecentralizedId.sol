edd17f50da3c46235b05674f5d884c3b74cbe21d
0JIEUN0 <jieun4436@naver.com>
2022-02-10 15:23:07 +0900

    Fix DecentralizedId contract: init owner

--- contracts/DecentralizedId.sol
+++ contracts/DecentralizedId.sol
@@ -18,15 +18,15 @@ contract DecentralizedId {
         _;
     }
 
-    constructor(address _owner) {
-        owner = _owner;
+    constructor() {
+        owner = msg.sender;
     }
 
     function checkRegistered(string memory _id) internal view returns (bool) {
         return ids[_id].isValid;
     }
 
-    function registerId(string memory _id) onlyOwner public {
+    function registerId(string memory _id) onlyOwner external {
         require(
             checkRegistered(_id) == false,
             "Already registered ID."

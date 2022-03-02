d39dd144d1fa5e1d9f6d77365cf590060f04ef3b
Jiwoo Hong <hjw6112@gmail.com>
2022-03-02 17:36:43 +0900

    feat: Group mutual-authentication

--- truffle-config.js
+++ truffle-config.js
@@ -40,7 +40,13 @@ module.exports = {
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
-    //
+    
+    development: {
+      host: "127.0.0.1",   
+      port: 7545,           
+      network_id: 5777,   
+    }
+
     // development: {
     //  host: "127.0.0.1",     // Localhost (default: none)
     //  port: 8545,            // Standard Ethereum port (default: none)

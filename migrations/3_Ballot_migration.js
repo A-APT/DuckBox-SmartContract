e0e3704f6e2a7ae944bd975189e8b9d61572093d
0JIEUN0 <je300512@gmail.com>
2022-03-22 01:10:44 +0900

    Add Ballots constract: for handling multiple Ballot with ballotId

--- migrations/3_Ballot_migration.js
+++ migrations/3_Ballot_migration.js
@@ -1,8 +1,10 @@
-const Ballot = artifacts.require("Ballot");
+// const Ballot = artifacts.require("Ballot");
+const Ballots = artifacts.require("Ballots");
 
 let startTime = Math.floor(Date.now() / 1000)
 let endTime = startTime + 100
 
 module.exports = function (deployer) {
-    deployer.deploy(Ballot, ["candidate1"], true, startTime, endTime, ["voter1", "voter2"]);
+    // deployer.deploy(Ballot, ["candidate1"], true, startTime, endTime, ["voter1", "voter2"]);
+    deployer.deploy(Ballots);
 };

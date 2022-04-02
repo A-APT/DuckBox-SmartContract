29f3d9f5705508cfa5bb51d4e2f97d2dc1301933
0JIEUN0 <je300512@gmail.com>
2022-04-02 16:27:40 +0900

    Group: change to store owner did and groupId, and emit event when group status changed to VALID

--- migrations/4_Group_migration.js
+++ migrations/4_Group_migration.js
@@ -1,6 +1,7 @@
 const Group = artifacts.require("Group");
+let _groupId = "groupId";
 let _groupOwnerDid = "groupOwnerDid";
 
 module.exports = function (deployer){
-    deployer.deploy(Group, _groupOwnerDid);
+    deployer.deploy(Group, _groupId, _groupOwnerDid);
 };

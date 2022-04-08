979c9d123f6a5138e79173cadbb48ea5e105856b
Jiwoo Hong <hjw6112@gmail.com>
2022-04-08 14:01:20 +0900

    fix: Ballots string-to-bytes32

--- test/BallotsTest.js
+++ test/BallotsTest.js
@@ -1,14 +1,15 @@
-const truffleAssert = require('truffle-assertions')
-const ballots = artifacts.require("Ballots")
+const truffleAssert = require('truffle-assertions');
+const ballots = artifacts.require("Ballots");
+const ethers = require('ethers');
 
 contract("Ballots", function (accounts) {
-    let owner = accounts[0]
-    let instance = null
-    let ballotId = "ballot id"
-    let startTime = Math.floor(Date.now() / 1000)
-    let endTime = startTime + 100
-    let candidates = ["candidate1", "candidate2"]
-    let voters = ["voter1", "voter2"]
+    let owner = accounts[0];
+    let instance = null;
+    let ballotId = "ballot id";
+    let startTime = Math.floor(Date.now() / 1000);
+    let endTime = startTime + 100;
+    let candidates = ["candidate1", "candidate2"];
+    let voters = [ethers.utils.formatBytes32String("voter1"), ethers.utils.formatBytes32String("voter2")];
 
     it("is_constructor_works_well", async function () {
         // get instance first
@@ -16,18 +17,20 @@ contract("Ballots", function (accounts) {
 
         // assert
         assert.equal(await instance.owner(), owner);
-    })
+    });
+    
     it("is_registerBallot_and_getBallot_works_well", async () => {
         // arrange
-        let chairperson = accounts[1]
+        let chairperson = accounts[1];
 
         // act
         await instance.registerBallot(
             ballotId, candidates, true, startTime, endTime, voters,
             {from: chairperson}
-        )
-        await instance.getBallot(ballotId)
-    })
+        );
+        await instance.getBallot(ballotId);
+    });
+
     it("is_registerBallot_reverts_duplicate_ballot", async () => {
         // act & assert
         await truffleAssert.reverts(
@@ -36,12 +39,13 @@ contract("Ballots", function (accounts) {
             ),
             "Already registered ballot (id)."
         );
-    })
+    });
+
     it("is_getBallot_reverts_invalid_ballotId", async () => {
         // act & assert
         await truffleAssert.reverts(
             instance.getBallot("invalid ballot id"),
             "Unregistered ballot (id)."
         );
-    })
-})
+    });
+});
